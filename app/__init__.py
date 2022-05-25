7e2ebfab4ea7437bfcab5693ee1128c989869e59
taylorstjean <69504992+taylorstjean@users.noreply.github.com>
2022-05-24 22:05:07 -0700

    Add files via upload

---
+++ app/__init__.py
@@ -0,0 +1,7 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+from app.mod_routing.controllers import mod_routing as routing_module
+
+app.register_blueprint(routing_module)
---
+++ app/mod_routing/controllers.py
@@ -0,0 +1,27 @@
+from flask import redirect, Blueprint, render_template, send_file
+
+mod_routing = Blueprint('routing', __name__)
+
+@mod_routing.route('/', methods=["GET", "POST"])
+def redirect_to_landing():
+    return redirect('/home/', 301)
+
+@mod_routing.route('/home/', methods=['GET', 'POST'])
+def landing_page():
+    return render_template("mod_routing/aboutme.html")
+
+@mod_routing.route('/contact/', methods=['GET', 'POST'])
+def contact_page():
+    return render_template("mod_routing/contactme.html")
+
+@mod_routing.route('/education/', methods=['GET', 'POST'])
+def education_page():
+    return render_template("mod_routing/education.html")
+
+@mod_routing.route('/experience/', methods=['GET', 'POST'])
+def experience_page():
+    return render_template("mod_routing/experience.html")
+
+@mod_routing.route('/download/', methods=['GET'])
+def download_resume():
+    return send_file('static/taylorstjean_resume.pdf', as_attachment=True)
