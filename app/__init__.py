38a212d594dadafeb25c894b9b998289998f5907
taylorstjean <69504992+taylorstjean@users.noreply.github.com>
2022-05-28 21:27:52 -0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, static_folder='static', static_url_path='')
+app = Flask(__name__, static_folder='static')
 
 from app.mod_routing.controllers import mod_routing as routing_module
 
