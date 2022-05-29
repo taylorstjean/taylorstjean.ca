91a9bd2029434041c917417a4f107d882ed3b2a2
taylorstjean <69504992+taylorstjean@users.noreply.github.com>
2022-05-29 15:24:19 -0700

    Update controllers.py

--- app/mod_routing/controllers.py
+++ app/mod_routing/controllers.py
@@ -1,22 +1,31 @@
 from flask import redirect, Blueprint, render_template
+from app.mod_routing.forms import ContactForm
 
 mod_routing = Blueprint('routing', __name__)
+
 
 @mod_routing.route('/', methods=["GET", "POST"])
 def redirect_to_landing():
     return redirect('/home/', 301)
 
+
 @mod_routing.route('/home/', methods=['GET', 'POST'])
 def landing_page():
     return render_template("mod_routing/aboutme.html")
 
+
 @mod_routing.route('/contact/', methods=['GET', 'POST'])
 def contact_page():
-    return render_template("mod_routing/contactme.html")
+    form = ContactForm()
+    if form.validate_on_submit():
+        print('success')
+    return render_template("mod_routing/contactme.html", form=form)
+
 
 @mod_routing.route('/education/', methods=['GET', 'POST'])
 def education_page():
     return render_template("mod_routing/education.html")
+
 
 @mod_routing.route('/experience/', methods=['GET', 'POST'])
 def experience_page():
